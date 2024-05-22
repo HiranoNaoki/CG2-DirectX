bb6016ae70188bb7db08a3abaac1f4d54e4181b0
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2024-05-22 14:38:59 +0900

    0202途中

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,3 +1,8 @@
+struct TransformationMatrix{
+float32_t4x4 WVP;
+}
+
+ConstanBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 struct VertexShaderOutput{
 float32_t4 position : SV_POSITION;
 
@@ -9,6 +14,6 @@ float32_t4 position : POSITION0;
 
 VertexShaderOutput main(VertexShaderInput input) {
 VertexShaderOutput output;
-output.position = input.position;
+output.position = mul(input.position,gTransformationMatrix.WVP);
 return output;
 }
